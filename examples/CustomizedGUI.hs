b1c69a0f43cc7bb6c3dc2c3f232a18a501d7bcab
Aleksey <astynax84@gmail.com>
2015-10-20 23:03:25 +0300

    More complex example

--- examples/CustomizedGUI.hs
+++ examples/CustomizedGUI.hs
@@ -7,9 +7,20 @@ import Prelude hiding (interact)
 import UI.Dialogui
 import UI.Dialogui.HTk
 
+type RGB = (Int, Int, Int)
+
+theme :: (HasColour a, HasFont a) => [Config a]
+theme = [ font (Courier, Bold, 18::Int)
+        , background ((0x00, 0x00, 0x7F) :: RGB)
+        , foreground ((0xFF, 0xFF, 0x00) :: RGB) ]
+
 main :: IO ()
-main = runGUIWith opts [] $ interact id
+main = runGUIWith opts
+       (writeLn "  -=<[ He110! ]>=-")
+       $ interact (\s -> "> " ++ s ++ "\n")
   where
     opts = defaultOptions { prepareWindow = set [ text "Customized!" ]
-                          , prepareOutput = set [ size (80, 25) ]}
+                          , prepareInput  = set   theme
+                          , prepareOutput = set $ theme ++ [ size (20, 10) ]
+                          }
     set = (void .) . flip configure
